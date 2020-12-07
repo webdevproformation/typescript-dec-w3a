c5f2c62e1ad807a312b5c14095ff5da9825e92fa
malik <malik.h@webdevpro.net>
2020-12-07 15:12:53 +0100

    fin enonce

--- 16-exo.ts
+++ 16-exo.ts
@@ -7,7 +7,6 @@ class Recette{
 
 let recettes : Array<Recette> = [];
 
-/* 1 Créez une variable « notes », 
-2 précisez le type de cette variable dans sa définition (Array de
-« number »). 
+/* 1 Créez une variable « recettes », 
+2 précisez le type de cette variable dans sa définition (Array<Recette>). 
 3 Ajoutez quelques valeurs dans ce tableau et affichez en console le contenu de celle-ci. */
