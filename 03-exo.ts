8c005b4e4edff06eaa188a4c335d6b95b0d84234
malik <malik.h@webdevpro.net>
2020-12-07 10:28:31 +0100

    exo

---
+++ 03-exo.ts
@@ -0,0 +1,21 @@
+// dans ce fichier réaliser le cas pratique suivant :
+
+/**
+// ce fichier va contenir plusieurs variables
+// veuillez les typer les variables suivantes correctement :
+
+// liste1 = "pêche poire ananas";
+// liste2 = ["pêche" , "poire", "ananas"];
+// liste3 = ["pêche", 4, "poire", 3, "ananas" , 2];
+/* liste4 = {
+    items : ["pêche", 4, "poire", 3, "ananas" , 2],
+    budget : 40 ,
+    unite : "euros"
+};*/
+/* liste5 = [
+    {id: 1 , nom : "pêche" , quantite : 4},
+    {id: 2 , nom : "poire" , quantite : 3},
+    {id: 3 , nom : "ananas" , quantite : 2},
+];*/
+
+// pouvez donner le bon typage pour chaque variables créées
