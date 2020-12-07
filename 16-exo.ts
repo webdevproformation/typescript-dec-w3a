6b6e83c2084e7823f0ab3eb2b14d82b94515abfa
malik <malik.h@webdevpro.net>
2020-12-07 15:11:55 +0100

    enonce

---
+++ 16-exo.ts
@@ -0,0 +1,13 @@
+// ce fichier contient le code suivant :
+
+class Recette{
+    name: string;
+    note?: number ;
+}
+
+let recettes : Array<Recette> = [];
+
+/* 1 Créez une variable « notes », 
+2 précisez le type de cette variable dans sa définition (Array de
+« number »). 
+3 Ajoutez quelques valeurs dans ce tableau et affichez en console le contenu de celle-ci. */
