5c8e5ac3f3771ef03a507713bcc60a051d58eb45
malik <malik.h@webdevpro.net>
2020-12-07 14:34:39 +0100

    interface et class

--- 13-class-interface.ts
+++ 13-class-interface.ts
@@ -1,1 +1,33 @@
 // 13-class-interface.ts
+
+// lorsque l'on veut typer un objet litteral de manière précise 
+// on crée un interface 
+
+interface Panier{
+    total : number,
+    listeProduit : Array<string> ,
+    devise : string
+}
+
+let p:Panier = {
+    total : 1000,
+    listeProduit : ["Produit1","Produit2"],
+    devise : "Euros"
+}
+// interface permettent de typer les objets littéraux 
+// 1ère utilisation des interfaces 
+
+// 2ème utilisation possible implémenter les class 
+
+class PanierAchat implements Panier{
+    // il faut obligatoirement que cette class dispose de toutes propriétés de l'interface = interface == class à 100% abstraite
+    total : number;
+    listeProduit : Array<string> ;
+    devise : string ;
+
+    private autreAttribut : boolean ;
+
+    methode(){}
+}
+
+// 
